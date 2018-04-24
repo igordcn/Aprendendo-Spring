2d6d2beb3048e969dcc77b0161178a88fbdad716
Igor Negocio <igorcnegocio@gmail.com>
2018-04-24 18:16:10 -0300

    Implementacao do service e repository de categoria, alimentacao automatica do banco e criacao do endpoint com id

--- src/main/java/com/example/curso/resource/CategoriaResource.java
+++ src/main/java/com/example/curso/resource/CategoriaResource.java
@@ -3,17 +3,29 @@ package com.example.curso.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.curso.domain.Categoria;
+import com.example.curso.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> buscar(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	public List<Categoria> listar() {
 		Categoria cat1 = new Categoria(1, "Informática");
 		Categoria cat2 = new Categoria(2, "Escritório");
@@ -22,4 +34,9 @@ public class CategoriaResource {
 		lista.add(cat2);
 		return lista;
 	}
+	
+	/*
+	 * categoriaRepository.saveAll(Arrays.asList(cat1, cat2) 
+	 * 
+	 * */
 }
---
+++ src/main/java/com/example/curso/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.example.curso.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.curso.domain.Categoria;
+import com.example.curso.repositories.CategoriaRepository;
+
+
+
+
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repositorio;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repositorio.findById(id);
+		return obj.orElse(null);
+	}
+
+}
